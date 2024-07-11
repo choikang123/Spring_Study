79cda69f52120e894f4b399fc314a36b69bbe27a
choikang <choikang7157@naver.com>
2024-07-11 23:31:53 +0900

    스프링 컨테이너 도입

--- hello-spring/src/main/java/hello/hello_spring/order/AppConfig.java
+++ hello-spring/src/main/java/hello/hello_spring/order/AppConfig.java
@@ -1,25 +1,32 @@
 package hello.hello_spring.order;
 
+import hello.hello_spring.discount.DiscountPolicy;
 import hello.hello_spring.discount.FixDiscountPolicy;
+import hello.hello_spring.discount.RateDiscountPolicy;
 import hello.hello_spring.member.MemberService;
 import hello.hello_spring.member.MemberServiceImpl;
 import hello.hello_spring.member.MeomoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memoryRepository());
     }
-
-    private static MeomoryMemberRepository memoryRepository() {
+    @Bean
+    public static MeomoryMemberRepository memoryRepository() {
         return new MeomoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memoryRepository(), discountPolicy());
     }
-
-    private static FixDiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public static DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 
--- hello-spring/src/main/java/hello/hello_spring/order/OrderApp.java
+++ hello-spring/src/main/java/hello/hello_spring/order/OrderApp.java
@@ -1,12 +1,18 @@
 package hello.hello_spring.order;
 
 import hello.hello_spring.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
 
         Member member = new Member(1L, "choi", Grade.VIP);
         memberService.join(member);
