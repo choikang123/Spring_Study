9d9129a7e74a7e3d1ae382776ef4d1de9ff208db
choikang <choikang7157@naver.com>
2024-07-29 20:46:30 +0900

    스프링 빈

---
+++ hello-spring/src/test/java/hello/hello_spring/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,34 @@
+package hello.hello_spring.beanfind;
+
+import hello.hello_spring.member.MemberService;
+import hello.hello_spring.member.MemberServiceImpl;
+import hello.hello_spring.order.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByNameX() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+}
